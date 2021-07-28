7f6bcc3b8129ecddfd896e06ec9f1e0fe1cac112
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-28 16:52:57 -0300

    Results Component

---
+++ src/components/Results.js
@@ -0,0 +1,5 @@
+const Results = () => {
+  return <div>Hello I am from result component</div>;
+};
+
+export default Results;
--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,27 +1,32 @@
+import Results from './Results';
+
 const SearchArea = () => {
   const keyword = 'football';
 
   return (
-    <form
-      style={{ maxWidth: '50%' }}
-      className="mt-5 mx-auto flex flex-col items-center justify-between"
-    >
-      <label
-        htmlFor="keyword"
-        className="pt-5 font-medium text-black text-center text-xl"
+    <>
+      <form
+        style={{ maxWidth: '50%' }}
+        className="mt-5 mx-auto flex flex-col items-center justify-between"
       >
-        Search
-      </label>
-      <input
-        type="text"
-        id="keyword"
-        value={keyword}
-        className="w-full text-center py-2 border mt-3 border-gray-300 rounded-md mb-4"
-      />
-      <button className="w-full bg-indigo-600 text-white p-2 rounded-md mb-4">
-        Submit
-      </button>
-    </form>
+        <label
+          htmlFor="keyword"
+          className="pt-5 font-medium text-black text-center text-xl"
+        >
+          Search
+        </label>
+        <input
+          type="text"
+          id="keyword"
+          value={keyword}
+          className="w-full text-center py-2 border mt-3 border-gray-300 rounded-md mb-4"
+        />
+        <button className="w-full bg-indigo-600 text-white p-2 rounded-md mb-4">
+          Submit
+        </button>
+      </form>
+      <Results />
+    </>
   );
 };
 
