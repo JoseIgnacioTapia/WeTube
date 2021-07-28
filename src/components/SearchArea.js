86a32679da21e3b2cd8ec7875f4bfc1886035a9d
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-28 17:06:05 -0300

    Adding useState and Events on Form Component

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Results from './Results';
 
 const SearchArea = () => {
-  const keyword = 'football';
+  const [keyword, setKeyword] = useState('');
+
+  const requestSearch = () => {
+    console.log('submitted');
+  };
 
   return (
     <>
       <form
         style={{ maxWidth: '50%' }}
         className="mt-5 mx-auto flex flex-col items-center justify-between"
+        onSubmit={e => {
+          e.preventDefault();
+          requestSearch();
+        }}
       >
         <label
           htmlFor="keyword"
@@ -17,9 +26,10 @@ const SearchArea = () => {
         </label>
         <input
           type="text"
+          className="w-full text-center py-2 border mt-3 border-gray-300 rounded-md mb-4"
           id="keyword"
           value={keyword}
-          className="w-full text-center py-2 border mt-3 border-gray-300 rounded-md mb-4"
+          onChange={e => setKeyword(e.target.value)}
         />
         <button className="w-full bg-indigo-600 text-white p-2 rounded-md mb-4">
           Submit
