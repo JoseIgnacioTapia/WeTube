4c117e11a4824feb7966e3be62ec790f5dfbd5a1
JoseIgnacioTapia <josignaciot@gmail.com>
2021-08-02 17:56:43 -0300

    Adding Button

--- src/components/Video.js
+++ src/components/Video.js
@@ -15,8 +15,14 @@ const Video = ({ video }) => {
       <div className="px-4 py-2">
         <h2 className="font-bold">{title}</h2>
         <small className="italic text-sm">{dateFormated}</small>
-        <h3 className="text-sm	 py-2">{channel}</h3>
-        <p className="text-base text-gray-500">{description}</p>
+        <div className="flex justify-between items-center	my-4">
+          <h3 className="text-sm	 py-2">{channel}</h3>
+          <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Watch Video
+          </button>
+        </div>
+        {/* 
+        <p className="text-base text-gray-500">{description}</p> */}
       </div>
     </div>
   );
